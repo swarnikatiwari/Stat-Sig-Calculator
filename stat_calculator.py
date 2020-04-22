5e9c6e9860ee7487cb8216028c1ff6e1b7dab12c
Hrishi <htiwari1@gmail.com>
2020-04-22 19:34:30 -0400

    removed duplicate app init

--- stat_calculator.py
+++ stat_calculator.py
@@ -14,7 +14,7 @@ colors = dict(background='#111111', text='#008080')
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
+server = app.server
 
 ALLOWED_TYPES = ("number", "number")
 
@@ -39,13 +39,13 @@ app.layout = html.Div(
 )
 
 def update_output_div(conversion_test, sample_test, conversion_control, sample_control):
-    # b = (float(conversion_test) / float(sample_test)) * 100
-    # a = (float(conversion_control) / float(sample_control)) * 100
-    # obs = np.array([[sample_control, conversion_control], [sample_test, conversion_test]])
-    # g, p, dof, expctd = chi2_contingency(obs, correction=False)
-    a = 0
-    b = 1
-    p = 2
+    b = (float(conversion_test) / float(sample_test)) * 100
+    a = (float(conversion_control) / float(sample_control)) * 100
+    obs = np.array([[sample_control, conversion_control], [sample_test, conversion_test]])
+    g, p, dof, expctd = chi2_contingency(obs, correction=False)
+    # a = 0
+    # b = 1
+    # p = 2
     return """Control Conversion Rate is: %.2f %% and Test Conversion Rate is: %.2f %%
 			\n The p-value of this test is: %f""" % (a, b, p)
 
