89bd7b21b515abf4d7827469f5900af5b720507d
Hrishi <htiwari1@gmail.com>
2020-04-22 19:32:28 -0400

    removed duplicate app init

--- stat_calculator.py
+++ stat_calculator.py
@@ -37,14 +37,15 @@ app.layout = html.Div(
         [Input('conversion_test', 'value'), Input('sample_test', 'value'),
          Input('conversion_control', 'value'), Input('sample_control', 'value')]
 )
+
 def update_output_div(conversion_test, sample_test, conversion_control, sample_control):
     # b = (float(conversion_test) / float(sample_test)) * 100
     # a = (float(conversion_control) / float(sample_control)) * 100
     # obs = np.array([[sample_control, conversion_control], [sample_test, conversion_test]])
     # g, p, dof, expctd = chi2_contingency(obs, correction=False)
-    a = '0'
-    b = '1'
-    p = '2'
+    a = 0
+    b = 1
+    p = 2
     return """Control Conversion Rate is: %.2f %% and Test Conversion Rate is: %.2f %%
 			\n The p-value of this test is: %f""" % (a, b, p)
 
