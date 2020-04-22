9f1a8676445b8ff577e87fa682910c5b613c84c6
Hrishi <htiwari1@gmail.com>
2020-04-22 19:26:54 -0400

    removed duplicate app init

--- stat_calculator.py
+++ stat_calculator.py
@@ -6,14 +6,14 @@ import plotly.graph_objs as go
 import numpy as np
 from scipy.stats import chi2_contingency, fisher_exact
 
-app = dash.Dash()
+# app = dash.Dash()
 
 # define colors to standardize color calls throughout the dashboard
 colors = dict(background='#111111', text='#008080')
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 ALLOWED_TYPES = ("number", "number")
@@ -38,10 +38,13 @@ app.layout = html.Div(
          Input('conversion_control', 'value'), Input('sample_control', 'value')]
 )
 def update_output_div(conversion_test, sample_test, conversion_control, sample_control):
-    b = (float(conversion_test) / float(sample_test)) * 100
-    a = (float(conversion_control) / float(sample_control)) * 100
-    obs = np.array([[sample_control, conversion_control], [sample_test, conversion_test]])
-    g, p, dof, expctd = chi2_contingency(obs, correction=False)
+    # b = (float(conversion_test) / float(sample_test)) * 100
+    # a = (float(conversion_control) / float(sample_control)) * 100
+    # obs = np.array([[sample_control, conversion_control], [sample_test, conversion_test]])
+    # g, p, dof, expctd = chi2_contingency(obs, correction=False)
+    a = '0'
+    b = '1'
+    p = '2'
     return """Control Conversion Rate is: %.2f %% and Test Conversion Rate is: %.2f %%
 			\n The p-value of this test is: %f""" % (a, b, p)
 
