c79dfe3b5682f01fc49b812de5253b221317bcfd
Hrishi <htiwari1@gmail.com>
2020-04-22 19:21:11 -0400

    removed duplicate app init

--- stat_calculator.py
+++ stat_calculator.py
@@ -6,14 +6,14 @@ import plotly.graph_objs as go
 import numpy as np
 from scipy.stats import chi2_contingency, fisher_exact
 
-# app = dash.Dash()
+app = dash.Dash()
 
 # define colors to standardize color calls throughout the dashboard
 colors = dict(background='#111111', text='#008080')
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 ALLOWED_TYPES = ("number", "number")
